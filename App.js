0b3811d604a88738cb74747b9535144f62cb392c
aarya-ghimire <aaryaghimire36@gmail.com>
2025-01-05 23:11:44 +0545

    added some greetings

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
+      <Text style={styles.greeting}>Welcome to My App!</Text>
+      <Text style={styles.message}>
+        Open up App.js to start working on your app
+      </Text>
       <StatusBar style="dark" />
     </View>
   );
@@ -13,8 +16,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f4f3ee",
     alignItems: "center",
     justifyContent: "center",
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#264653",
+  },
+  message: {
+    fontSize: 16,
+    backgroundColor: "#fed9b7",
+    padding: 10,
+    borderRadius: 5,
+    textAlign: "center",
+    color: "#001219",
   },
 });
